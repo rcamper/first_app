192b7da98683d7edb12fb71ea608d9e5cdc8108c
Robert Camper <rcamper@healthplancrm.com>
2014-01-21 16:48:32 -0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'dfbad464f3219d624ae3ad552a54a9ebc987e2cd4f319c55d2dce31cf04ad7861c9b0fb49063c487ea866ab9150ad87e8a29aa9af4c38ae857c6e003b27d6cbb'
